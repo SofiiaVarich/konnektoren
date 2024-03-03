472264978d1a5cc9297ec73b6ec75c82cfaaf8fb
Christian M <chriamue@gmail.com>
2024-03-03 13:43:42 +0100

    adds bootstrap

--- src/app.rs
+++ src/app.rs
@@ -1,10 +1,13 @@
 use yew::prelude::*;
+use yew_bootstrap::util::*;
 
 #[function_component]
 pub fn App() -> Html {
     html! {
         <div>
+            {include_cdn()}
             <h1>{"Konnektoren"}</h1>
+            {include_cdn_js()}
         </div>
     }
 }
