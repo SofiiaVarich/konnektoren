015217a280532642b86c913dccf87b25efe431a3
Christian M <chriamue@gmail.com>
2024-03-06 19:36:58 +0100

    adds robots.txt and sitemap.xml

---
+++ scripts/generate_sitemap.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DOMAIN="https://konnektoren.help"
+
+CURRENT_DATE=$(date +%Y-%m-%d)
+
+PAGES=("/" "/about")
+
+echo '<?xml version="1.0" encoding="UTF-8"?>' > sitemap.xml
+echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' >> sitemap.xml
+
+for PAGE in "${PAGES[@]}"; do
+  echo "  <url>" >> sitemap.xml
+  echo "    <loc>${DOMAIN}${PAGE}</loc>" >> sitemap.xml
+  echo "    <lastmod>${CURRENT_DATE}</lastmod>" >> sitemap.xml
+  echo "  </url>" >> sitemap.xml
+done
+
+echo '</urlset>' >> sitemap.xml
