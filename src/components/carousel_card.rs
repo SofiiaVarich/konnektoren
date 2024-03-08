9ab3aff60d807934e9107576f5081960d77f9e4c
Christian M <chriamue@gmail.com>
2024-03-08 16:29:30 +0100

    :bug: fix carousel content

--- src/components/carousel_card.rs
+++ src/components/carousel_card.rs
@@ -10,6 +10,7 @@ pub struct ModelProps<D: DetailTrait> {
 
 pub struct CarouselCard<D: DetailTrait> {
     detail: D,
+    pub hide_example: Option<bool>,
 }
 
 pub enum Msg {}
@@ -21,11 +22,22 @@ impl<D: DetailTrait + 'static> Component for CarouselCard<D> {
     fn create(ctx: &Context<Self>) -> Self {
         Self {
             detail: ctx.props().detail.clone(),
+            hide_example: ctx.props().hide_example.clone(),
         }
     }
 
-    fn view(&self, ctx: &Context<Self>) -> Html {
-        let example = if ctx.props().hide_example.is_some_and(|x| x == true) {
+    fn changed(&mut self, ctx: &Context<Self>, _old_props: &Self::Properties) -> bool {
+        if self.detail != ctx.props().detail || self.hide_example != ctx.props().hide_example {
+            self.detail = ctx.props().detail.clone();
+            self.hide_example = ctx.props().hide_example.clone();
+            true
+        } else {
+            false
+        }
+    }
+
+    fn view(&self, _ctx: &Context<Self>) -> Html {
+        let example = if self.hide_example.is_some_and(|x| x == true) {
             html! {}
         } else {
             html! {
