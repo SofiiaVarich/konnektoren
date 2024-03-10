281b5b86e205fb4250a433fffe681238eb5175f5
Christian M <chriamue@gmail.com>
2024-03-10 08:32:58 +0100

    adds link copied to clipboard information

--- src/pages/results.rs
+++ src/pages/results.rs
@@ -1,9 +1,11 @@
 use crate::model::TestResult;
 use crate::pages::Route;
+use urlencoding::encode;
 use web_sys::window;
 use yew::prelude::*;
 use yew_hooks::{use_clipboard, UseClipboardHandle};
 use yew_router::prelude::*;
+use gloo_timers::callback::Timeout;
 
 #[derive(Properties, PartialEq)]
 pub struct ResultsProps {
@@ -13,6 +15,7 @@ pub struct ResultsProps {
 #[function_component(Results)]
 pub fn results(props: &ResultsProps) -> Html {
     let clipboard_handle: UseClipboardHandle = use_clipboard();
+    let show_copied_message = use_state(|| false);
 
     let code: String = props.code.as_ref().unwrap_or(&"".to_string()).clone();
     let test_result = TestResult::from_base64(&code.clone());
@@ -22,18 +25,20 @@ pub fn results(props: &ResultsProps) -> Html {
 
     match test_result {
         Ok(test_result) => {
-            let share_url = format!(
-                "{}//{}/?page=results&code={}",
-                protocol,
-                host,
-                test_result.to_base64()
-            );
+            let encoded_code: String = encode(&test_result.to_base64()).into_owned();
+            let share_url = format!("{}//{}/?page=results&code={}", protocol, host, encoded_code);
 
             let on_share_click = {
                 let clipboard_handle = clipboard_handle.clone();
                 let data = share_url.clone();
+                let show_copied_message = show_copied_message.clone();
                 Callback::from(move |_| {
                     clipboard_handle.write_text(data.to_string());
+                    show_copied_message.set(true);
+                    let show_copied_message = show_copied_message.clone();
+                    Timeout::new(3000, move || {
+                        show_copied_message.set(false);
+                    }).forget();
                 })
             };
 
@@ -53,6 +58,9 @@ pub fn results(props: &ResultsProps) -> Html {
                     </div>
                     <input type="text" class="share-url-input" readonly=true value={share_url.clone()} />
                     <button onclick={on_share_click}>{ "Share This Result" }</button>
+                    if *show_copied_message {
+                        <p class="copied-message">{"Link copied to clipboard!"}</p>
+                    }
                     <div class="try-again">
                     <Link<Route> to={Route::Home}>{ "Try the test yourself again!" }</Link<Route>>
                 </div>
