df06ef10e9646851b3870fdc90e945c68b7e06e5
Christian M <chriamue@gmail.com>
2024-03-22 07:30:44 +0100

    url decode code

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
-use worker::*;
-use std::io::Cursor;
-use konnektoren::utils::create_certificate;
 use image::ImageOutputFormat;
 use konnektoren::model::TestResult;
+use konnektoren::utils::create_certificate;
+use std::io::Cursor;
+use urlencoding::decode;
+use worker::*;
 
 #[event(fetch)]
 pub async fn main(req: Request, _env: Env, _ctx: Context) -> Result<Response> {
@@ -14,11 +15,14 @@ pub async fn main(req: Request, _env: Env, _ctx: Context) -> Result<Response> {
     let url = req.url()?;
     let path = url.path();
     let base64_encoded = match path.strip_prefix("/certificate/") {
-        Some(encoded) => encoded.trim_end_matches(".png"),
+        Some(encoded) => {
+            let data = encoded.trim_end_matches(".png");
+            decode(data).unwrap_or_default().to_string()
+        }
         None => return Response::error("Bad Request", 400),
     };
 
-    let test_result = match TestResult::from_base64(base64_encoded) {
+    let test_result = match TestResult::from_base64(&base64_encoded) {
         Ok(result) => result,
         Err(_) => return Response::error(format!("Bad Request {}", base64_encoded), 400),
     };
@@ -32,15 +36,14 @@ pub async fn main(req: Request, _env: Env, _ctx: Context) -> Result<Response> {
             match image.write_to(&mut Cursor::new(&mut bytes), ImageOutputFormat::Png) {
                 Ok(_) => {
                     // Create a response with the PNG data
-                    Response::from_bytes(bytes)
-                        .map(|mut res| {
-                            let _ = res.headers_mut().set("Content-Type", "image/png");
-                            res
-                        })
-                },
+                    Response::from_bytes(bytes).map(|mut res| {
+                        let _ = res.headers_mut().set("Content-Type", "image/png");
+                        res
+                    })
+                }
                 Err(_) => Response::error("Internal Server Error", 500),
             }
-        },
+        }
         Err(_) => Response::error("Internal Server Error", 500),
     }
 }
