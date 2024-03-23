1c4449265dce17cc609c5e324751a408b45f2670
Christian M <chriamue@gmail.com>
2024-03-23 07:22:25 +0100

    refactors type selector

--- src/components/type_selector.rs
+++ src/components/type_selector.rs
@@ -1,13 +1,14 @@
-use crate::model::{AdjectiveType, KonnektorType, NomenType, TypeTrait, VerbType};
+use crate::model::TypeTrait;
 use rand::seq::SliceRandom;
 use yew::prelude::*;
 use yew_bootstrap::component::{Button, ButtonGroup};
+
 #[derive(Properties, PartialEq)]
 pub struct TypeSelectorProps<T: TypeTrait> {
     pub on_select: Callback<T>,
 }
 
-pub struct TypeSelector<T: TypeTrait> {
+pub struct TypeSelector<T: TypeTrait + 'static> {
     button_order: Vec<T>,
 }
 
@@ -15,28 +16,22 @@ pub enum Msg<T: TypeTrait> {
     ButtonClicked(T),
 }
 
-impl Component for TypeSelector<KonnektorType> {
-    type Message = Msg<KonnektorType>;
-    type Properties = TypeSelectorProps<KonnektorType>;
+impl<T: TypeTrait> Component for TypeSelector<T> {
+    type Message = Msg<T>;
+    type Properties = TypeSelectorProps<T>;
 
     fn create(_ctx: &Context<Self>) -> Self {
         Self {
-            button_order: vec![
-                KonnektorType::Subjunktionen,
-                KonnektorType::Konjunktionen,
-                KonnektorType::Konjunktionaladverbien,
-                KonnektorType::Infinitivgruppe,
-                KonnektorType::BesonderePosition,
-            ],
+            button_order: T::iter().collect(),
         }
     }
 
     fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
         match msg {
-            Msg::ButtonClicked(konnektor_type) => {
+            Msg::ButtonClicked(t_type) => {
                 let mut rng = rand::thread_rng();
                 self.button_order.shuffle(&mut rng);
-                ctx.props().on_select.emit(konnektor_type);
+                ctx.props().on_select.emit(t_type);
             }
         }
         true
@@ -46,201 +41,11 @@ impl Component for TypeSelector<KonnektorType> {
         html! {
             <div id="typeselector" class="d-flex justify-content-center">
             <ButtonGroup>
-            {for self.button_order.iter().map(|konnektor_type| {
-                let button_text = match konnektor_type {
-                    KonnektorType::Subjunktionen => "Nebensatz (Verb am Ende)",
-                    KonnektorType::Konjunktionen => "Hauptsatz (Position 0)",
-                    KonnektorType::Konjunktionaladverbien => "Hauptsatz (Position 1)",
-                    KonnektorType::Infinitivgruppe => "Infinitivgruppe",
-                    KonnektorType::BesonderePosition => "Besondere Position",
-                };
-                let konnektor_type_clone = konnektor_type.clone();
+            {for self.button_order.iter().map(|t_type| {
+                let button_text = t_type.to_string();
+                let t_type_clone = t_type.clone();
                 html! {
-                    <Button onclick={ctx.link().callback(move |_| Msg::ButtonClicked(konnektor_type_clone.clone()))}>
-                        {button_text}
-                    </Button>
-                }
-            })}
-        </ButtonGroup>
-            </div>
-        }
-    }
-}
-
-impl Component for TypeSelector<AdjectiveType> {
-    type Message = Msg<AdjectiveType>;
-    type Properties = TypeSelectorProps<AdjectiveType>;
-
-    fn create(_ctx: &Context<Self>) -> Self {
-        Self {
-            button_order: vec![
-                AdjectiveType::An,
-                AdjectiveType::Auf,
-                AdjectiveType::Bei,
-                AdjectiveType::Fuer,
-                AdjectiveType::Mit,
-                AdjectiveType::Ueber,
-                AdjectiveType::Von,
-                AdjectiveType::Zu,
-            ],
-        }
-    }
-
-    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
-        match msg {
-            Msg::ButtonClicked(preposition_type) => {
-                let mut rng = rand::thread_rng();
-                self.button_order.shuffle(&mut rng);
-                ctx.props().on_select.emit(preposition_type);
-            }
-        }
-        true
-    }
-
-    fn view(&self, ctx: &Context<Self>) -> Html {
-        html! {
-            <div id="typeselector" class="d-flex justify-content-center">
-            <ButtonGroup>
-            {for self.button_order.iter().map(|preposition_type| {
-                let button_text = match preposition_type {
-                    AdjectiveType::An => "An",
-                    AdjectiveType::Auf => "Auf",
-                    AdjectiveType::Bei => "Bei",
-                    AdjectiveType::Fuer => "Für",
-                    AdjectiveType::Mit => "Mit",
-                    AdjectiveType::Ueber => "Über",
-                    AdjectiveType::Von => "Von",
-                    AdjectiveType::Zu => "Zu",
-                };
-                let preposition_type_clone = preposition_type.clone();
-                html! {
-                    <Button onclick={ctx.link().callback(move |_| Msg::ButtonClicked(preposition_type_clone.clone()))}>
-                        {button_text}
-                    </Button>
-                }
-            })}
-        </ButtonGroup>
-            </div>
-        }
-    }
-}
-
-impl Component for TypeSelector<VerbType> {
-    type Message = Msg<VerbType>;
-    type Properties = TypeSelectorProps<VerbType>;
-
-    fn create(_ctx: &Context<Self>) -> Self {
-        Self {
-            button_order: vec![
-                VerbType::An,
-                VerbType::Auf,
-                VerbType::Fuer,
-                VerbType::In,
-                VerbType::Mit,
-                VerbType::Nach,
-                VerbType::Ueber,
-                VerbType::Um,
-                VerbType::Unter,
-                VerbType::Von,
-                VerbType::Vor,
-                VerbType::Zu,
-            ],
-        }
-    }
-
-    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
-        match msg {
-            Msg::ButtonClicked(verb_type) => {
-                let mut rng = rand::thread_rng();
-                self.button_order.shuffle(&mut rng);
-                ctx.props().on_select.emit(verb_type);
-            }
-        }
-        true
-    }
-
-    fn view(&self, ctx: &Context<Self>) -> Html {
-        html! {
-            <div id="typeselector" class="d-flex justify-content-center">
-            <ButtonGroup>
-            {for self.button_order.iter().map(|verb_type| {
-                let button_text = match verb_type {
-                    VerbType::An => "An",
-                    VerbType::Auf => "Auf",
-                    VerbType::Fuer => "Für",
-                    VerbType::In => "In",
-                    VerbType::Mit => "Mit",
-                    VerbType::Nach => "Nach",
-                    VerbType::Ueber => "Über",
-                    VerbType::Um => "Um",
-                    VerbType::Unter => "Unter",
-                    VerbType::Von => "Von",
-                    VerbType::Vor => "Vor",
-                    VerbType::Zu => "Zu",
-                };
-                let verb_type_clone = verb_type.clone();
-                html! {
-                    <Button onclick={ctx.link().callback(move |_| Msg::ButtonClicked(verb_type_clone.clone()))}>
-                        {button_text}
-                    </Button>
-                }
-            })}
-        </ButtonGroup>
-            </div>
-        }
-    }
-}
-
-impl Component for TypeSelector<NomenType> {
-    type Message = Msg<NomenType>;
-    type Properties = TypeSelectorProps<NomenType>;
-
-    fn create(_ctx: &Context<Self>) -> Self {
-        Self {
-            button_order: vec![
-                NomenType::Bringen,
-                NomenType::Geraten,
-                NomenType::Kommen,
-                NomenType::Nehmen,
-                NomenType::Setzen,
-                NomenType::Stehen,
-                NomenType::Stellen,
-                NomenType::Stossen,
-                NomenType::Ziehen,
-            ],
-        }
-    }
-
-    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
-        match msg {
-            Msg::ButtonClicked(nomen_type) => {
-                let mut rng = rand::thread_rng();
-                self.button_order.shuffle(&mut rng);
-                ctx.props().on_select.emit(nomen_type);
-            }
-        }
-        true
-    }
-
-    fn view(&self, ctx: &Context<Self>) -> Html {
-        html! {
-            <div id="typeselector" class="d-flex justify-content-center">
-            <ButtonGroup>
-            {for self.button_order.iter().map(|nomen_type| {
-                let button_text = match nomen_type {
-                    NomenType::Bringen => "bringen",
-                    NomenType::Geraten => "geraten",
-                    NomenType::Kommen => "kommen",
-                    NomenType::Nehmen => "nehmen",
-                    NomenType::Setzen => "setzen",
-                    NomenType::Stehen => "stehen",
-                    NomenType::Stellen => "stellen",
-                    NomenType::Stossen => "stoßen",
-                    NomenType::Ziehen => "ziehen",
-                };
-                let nomen_type_clone = nomen_type.clone();
-                html! {
-                    <Button onclick={ctx.link().callback(move |_| Msg::ButtonClicked(nomen_type_clone.clone()))}>
+                    <Button onclick={ctx.link().callback(move |_| Msg::ButtonClicked(t_type_clone.clone()))}>
                         {button_text}
                     </Button>
                 }
--- src/model/type_trait.rs
+++ src/model/type_trait.rs
@@ -1,7 +1,14 @@
 use serde::Serialize;
+use strum::IntoEnumIterator;
 
 pub trait TypeTrait:
-    PartialEq + Clone + std::default::Default + std::fmt::Display + std::fmt::Debug + Serialize
+    PartialEq
+    + Clone
+    + std::default::Default
+    + std::fmt::Display
+    + std::fmt::Debug
+    + Serialize
+    + IntoEnumIterator
 {
     fn get_type() -> String;
 }
