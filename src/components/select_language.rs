2fc1a09034d5d93ab347888a54b0c1dd4699f356
Christian M <chriamue@gmail.com>
2024-03-27 19:02:48 +0100

    fix select language options

--- src/components/select_language.rs
+++ src/components/select_language.rs
@@ -9,7 +9,7 @@ pub fn select_language() -> Html {
     let mut i18n = use_translation();
 
     let selected_language =
-        use_state(|| LocalStorage::get(LANGUAGE_KEY).unwrap_or_else(|_| "en".to_string()));
+        use_state(|| LocalStorage::get(LANGUAGE_KEY).unwrap_or_else(|_| "".to_string()));
 
     let _ = i18n.set_translation_language(&*selected_language);
 
@@ -32,6 +32,7 @@ pub fn select_language() -> Html {
             <p>
                 { i18n.t("Please select a language from the dropdown.") }
                 <select onchange={on_select_change} value={(*selected_language).clone()}>
+                    <option value="" selected={selected_language.is_empty()} disabled=true>{ i18n.t("Select Language") }</option>
                     { for languages().iter().map(|&lang| html! {
                         <option value={lang} selected={lang == *selected_language}>{format!("{} {}", flag(&lang), lang)}</option>
                     })}
