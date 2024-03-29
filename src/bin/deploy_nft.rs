6ab645b8ae79dad24ec818dcd5678085235d5d7e
Christian M <chriamue@gmail.com>
2024-03-29 08:42:59 +0100

    add create asset and collection

--- src/bin/deploy_nft.rs
+++ src/bin/deploy_nft.rs
@@ -1,6 +1,9 @@
 use bip39::Mnemonic;
 use dotenv::dotenv;
-use mpl_core::instructions::CreateV1Builder;
+use mpl_core::{
+    instructions::{CreateCollectionV1Builder, CreateV1Builder},
+    Asset,
+};
 use solana_client::rpc_client::RpcClient;
 use solana_sdk::{
     derivation_path::DerivationPath,
@@ -30,10 +33,33 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let metadata_uri = "https://konnektoren.help/nft-metadata.json";
 
-    match create_asset(&client, &payer, metadata_uri) {
+    let collection = Keypair::new();
+
+    let asset = Keypair::new();
+
+    match create_asset_in_collection(&client, &payer, &asset, &collection, metadata_uri) {
+        Ok(_) => println!("Collection account created successfully."),
+        Err(e) => eprintln!("Failed to create collection account: {}", e),
+    }
+
+    println!("Collection account {}", collection.pubkey().to_string());
+
+    /*
+    match create_asset(&client, &payer, &asset, metadata_uri) {
         Ok(_) => println!("Metadata account created successfully."),
         Err(e) => eprintln!("Failed to create metadata account: {}", e),
     }
+    */
+
+    println!("Asset account {}", asset.pubkey().to_string());
+
+    let asset_id = asset.pubkey();
+
+    let rpc_data = client.get_account_data(&asset_id)?;
+
+    let asset = Asset::from_bytes(&rpc_data).unwrap();
+
+    println!("{:?}", asset);
 
     Ok(())
 }
@@ -55,9 +81,61 @@ fn mnemonic_to_keypair(
 pub fn create_asset(
     client: &RpcClient,
     payer: &Keypair,
+    asset: &Keypair,
     metadata_uri: &str,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    let asset = Keypair::new();
+    let create_asset_ix = CreateV1Builder::new()
+        .asset(asset.pubkey())
+        .payer(payer.pubkey())
+        .name("Konnektoren Help".into())
+        .uri(metadata_uri.into())
+        .instruction();
+
+    let signers = vec![&asset, &payer];
+
+    let last_blockhash = client.get_latest_blockhash()?;
+
+    let create_asset_tx = Transaction::new_signed_with_payer(
+        &[create_asset_ix],
+        Some(&payer.pubkey()),
+        &signers,
+        last_blockhash,
+    );
+
+    let res = client.send_and_confirm_transaction(&create_asset_tx)?;
+
+    println!("Signature: {:?}", res);
+    Ok(())
+}
+
+pub fn create_asset_in_collection(
+    client: &RpcClient,
+    payer: &Keypair,
+    asset: &Keypair,
+    collection: &Keypair,
+    metadata_uri: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let create_collection_ix = CreateCollectionV1Builder::new()
+        .collection(collection.pubkey())
+        .payer(payer.pubkey())
+        .name("Konnektoren Help".into())
+        .uri(metadata_uri.into())
+        .instruction();
+
+    let signers = vec![&collection, &payer];
+
+    let last_blockhash = client.get_latest_blockhash()?;
+
+    let create_collection_tx = Transaction::new_signed_with_payer(
+        &[create_collection_ix],
+        Some(&payer.pubkey()),
+        &signers,
+        last_blockhash,
+    );
+
+    let res = client.send_and_confirm_transaction(&create_collection_tx)?;
+
+    println!("Signature: {:?}", res);
 
     let create_asset_ix = CreateV1Builder::new()
         .asset(asset.pubkey())
