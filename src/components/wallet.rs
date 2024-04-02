b9e8596ede1354dddeba1d96d9d8aa37e0573311
Christian M <chriamue@gmail.com>
2024-04-02 17:48:31 +0200

    implements solana wallet click

--- src/components/wallet.rs
+++ src/components/wallet.rs
@@ -1,8 +1,16 @@
 use crate::utils::translation::LANGUAGE_KEY;
 use gloo_storage::{LocalStorage, Storage};
+use wasm_bindgen::prelude::*;
 use web_sys::HtmlInputElement;
 use yew::prelude::*;
 use yew_i18n::use_translation;
+
+#[wasm_bindgen(
+    inline_js = "export function connectSolanaWallet() { return window.connectSolanaWallet(); }"
+)]
+extern "C" {
+    async fn connectSolanaWallet() -> JsValue;
+}
 
 #[derive(Properties, PartialEq)]
 pub struct WalletProps {
@@ -25,11 +33,26 @@ pub fn wallet(props: &WalletProps) -> Html {
         })
     };
 
+    let on_change = props.on_change.clone();
+    let on_click = Callback::from(move |_| {
+        let on_change = on_change.clone();
+        wasm_bindgen_futures::spawn_local(async move {
+            let address = connectSolanaWallet().await;
+            if !address.is_undefined() && !address.is_null() {
+                let address_str: String = address.as_string().unwrap_or_default();
+                web_sys::console::log_1(&format!("Wallet address: {}", address_str).into());
+                on_change.emit(address_str);
+            } else {
+                web_sys::console::log_1(&"Failed to connect to the wallet.".into());
+            }
+        });
+    });
+
     html! {
         <div class="wallet">
             <label for="account">{ i18n.t("Solana Account: ") }</label>
             <input id="account" type="text" value={props.address.clone()} oninput={on_input_change} placeholder="Solana Account Address" />
-            <button>{ i18n.t("Connect Wallet") }</button>
+            <button onclick={on_click}>{ i18n.t("Connect Wallet") }</button>
        </div>
     }
 }
