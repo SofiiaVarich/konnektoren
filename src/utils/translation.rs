117bf8086fa7ac54856909d82d96733467615d9d
Christian M <chriamue@gmail.com>
2024-04-02 21:16:47 +0200

    adds arabic language

--- src/utils/translation.rs
+++ src/utils/translation.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 
 pub fn languages() -> Vec<&'static str> {
-    vec!["en", "ua", "de", "cn"]
+    vec!["en", "ua", "ar", "de", "cn"]
 }
 
 pub fn translations() -> HashMap<String, serde_json::Value> {
@@ -11,11 +11,13 @@ pub fn translations() -> HashMap<String, serde_json::Value> {
     let de = serde_json::from_str(include_str!("../assets/i18n/de.json")).unwrap();
     let ua = serde_json::from_str(include_str!("../assets/i18n/ua.json")).unwrap();
     let cn = serde_json::from_str(include_str!("../assets/i18n/cn.json")).unwrap();
+    let ar = serde_json::from_str(include_str!("../assets/i18n/ar.json")).unwrap();
 
     translations.insert("en".to_string(), en);
     translations.insert("de".to_string(), de);
     translations.insert("ua".to_string(), ua);
     translations.insert("cn".to_string(), cn);
+    translations.insert("ar".to_string(), ar);
     translations
 }
 
@@ -25,6 +27,7 @@ pub fn flag(lang: &'static str) -> &'static str {
         "de" => "🇩🇪",
         "ua" => "🇺🇦",
         "cn" => "🇨🇳",
+        "ar" => "🇸🇦",
         _ => "🌐",
     }
 }
