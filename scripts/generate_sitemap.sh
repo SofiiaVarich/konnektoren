9cc12296bf2ab0b2bbd6aebb96dc49a70c6c78a3
Christian M <chriamue@gmail.com>
2024-04-14 21:36:33 +0200

    add lang header link and update generate sitemap for translations

--- scripts/generate_sitemap.sh
+++ scripts/generate_sitemap.sh
@@ -4,16 +4,30 @@ DOMAIN="https://konnektoren.help"
 
 CURRENT_DATE=$(date +%Y-%m-%d)
 
-PAGES=("/" "/about" "/adjectives" "/konnektoren" "/verbs" "/leaderboard" "/.well-known/did.json" "/?lang=en" "/?lang=ua" "/?lang=de" "/?lang=cn" "/?lang=ar")
+PAGES=("/" "/about" "/adjectives" "/konnektoren" "/verbs" "/leaderboard")
+LANGS=("ar" "cn" "de" "en" "es" "pl" "tr" "ua")
 
 echo '<?xml version="1.0" encoding="UTF-8"?>' > sitemap.xml
-echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' >> sitemap.xml
+echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">' >> sitemap.xml
 
 for PAGE in "${PAGES[@]}"; do
   echo "  <url>" >> sitemap.xml
   echo "    <loc>${DOMAIN}${PAGE}</loc>" >> sitemap.xml
   echo "    <lastmod>${CURRENT_DATE}</lastmod>" >> sitemap.xml
-  echo "  </url>" >> sitemap.xml
+
+  echo "    <xhtml:link" >> sitemap.xml
+  echo "      rel=\"alternate\"" >> sitemap.xml
+  echo "      hreflang=\"en\"" >> sitemap.xml
+  echo "      href=\"${DOMAIN}${PAGE}\"/>" >> sitemap.xml
+
+    for LANG in "${LANGS[@]}"; do
+        echo "    <xhtml:link" >> sitemap.xml
+        echo "      rel=\"alternate\"" >> sitemap.xml
+        echo "      hreflang=\"${LANG}\"" >> sitemap.xml
+        echo "      href=\"${DOMAIN}${PAGE}?lang=${LANG}\"/>" >> sitemap.xml
+    done
+
+    echo "  </url>" >> sitemap.xml
 done
 
 echo '</urlset>' >> sitemap.xml
