aee0cbf7514621f281165ea3a007056fe1d61953
Christian M <chriamue@gmail.com>
2024-05-18 09:23:47 +0200

    adds gtag event on pro activation

--- src/components/pro_version.rs
+++ src/components/pro_version.rs
@@ -1,3 +1,7 @@
+use gloo_utils::format::JsValueSerdeExt;
+use serde_json::json;
+use wasm_bindgen::{JsCast, JsValue};
+use web_sys::window;
 use yew::prelude::*;
 
 #[function_component(ProVersion)]
@@ -7,6 +11,24 @@ pub fn pro_version() -> Html {
     let on_activate_click = {
         let show_message = show_message.clone();
         Callback::from(move |_| {
+            // Trigger Google Analytics event
+            if let Some(window) = window() {
+                if let Some(gtag) = window.get("gtag") {
+                    if let Ok(gtag) = gtag.dyn_into::<js_sys::Function>() {
+                        let _ = gtag.call2(
+                            &JsValue::NULL,
+                            &JsValue::from("event"),
+                            &JsValue::from_serde(&json!({
+                                "send_to": "default",
+                                "event_category": "pro_version",
+                                "event_label": "Activate Pro Version Button",
+                                "value": 1
+                            }))
+                            .unwrap(),
+                        );
+                    }
+                }
+            }
             show_message.set(true);
         })
     };
